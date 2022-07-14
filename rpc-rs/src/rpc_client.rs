83e0d77cdf9ed51db814677ff30de898ec85cd8d
stevelr <steve@cosmonic.com>
2022-07-14 10:24:35 -0700

    flush in separate task
    Signed-off-by: stevelr <steve@cosmonic.com>

--- rpc-rs/src/rpc_client.rs
+++ rpc-rs/src/rpc_client.rs
@@ -478,8 +478,12 @@ impl RpcClient {
             }
         })
         .await?;
-        // flush after publish to reduce delay
-        let _ = self.client().flush().await;
+        let nc = self.client();
+        tokio::spawn(async move {
+            if let Err(error) = nc.flush().await {
+                error!(%error, "flush after publish");
+            }
+        });
         Ok(())
     }
 
@@ -526,7 +530,12 @@ impl RpcClient {
                         "failed sending rpc response",
                     );
                 }
-                let _ = self.client().flush().await;
+                let nc = self.client();
+                tokio::spawn(async move {
+                    if let Err(error) = nc.flush().await {
+                        error!(%error, "flush after publishing invocation response");
+                    }
+                });
             }
             Err(e) => {
                 // extremely unlikely that InvocationResponse would fail to serialize
